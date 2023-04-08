ed240752d9664faa6bb7194f0f21ffcdacc49369
Devanshu <devanshupanigrahi2004@gmail.com>
2023-04-08 12:07:44 +0530

    Vechicle speed detection

---
+++ speed detection.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+
+    _, thresh = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
